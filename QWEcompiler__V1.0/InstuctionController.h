982cc62c5af3c8857903e9f0aca17b92ee17a720
gms <makno5ov@yandex.ru>
2025-08-09 22:18:02 +0300

    Доделал InstructionController - надо добавить работу с директориями

--- QWEcompiler__V1.0/InstuctionController.h
+++ QWEcompiler__V1.0/InstuctionController.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <fstream>
 #include <vector>
 #include "DEFAUT_VALUES.h"
 
@@ -8,10 +9,16 @@ class InstuctionController
 public:
 	void compute(std::vector<Instuction>* instuctions, const std::string& file_name)
 	{
+		std::ofstream file(file_name + ".qwe");
+
+		if( !file.is_open() ) throw compile_error("can`t create .qwe file" );
+
 		for (const auto& instruction : *instuctions)
 		{
-
+			file << instruction.GET_spell().use_spell() << "\n";
 		}
+
+		file.close();
 	}
 };
 
